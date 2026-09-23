35b453ddb96eecc646ac60a44b95186ef94482ab
Jakub Cieślik <kuba8172@gmail.com>
2026-09-23 16:17:10 +0200

    feature: product table with pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+import ProductsTable from "@/features/products/components/table/products-table";
+
 export default function Home() {
   return (
     <main className="mx-auto w-full max-w-[1240px] px-4 py-12">
       <h1 className="text-xl font-semibold">Produkty</h1>
+      <Suspense>
+        <ProductsTable />
+      </Suspense>
     </main>
   );
 }
