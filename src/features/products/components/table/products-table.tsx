bfc187cf3128030245f3822c4e5762622f5acb28
Jakub Cieślik <kuba8172@gmail.com>
2026-09-25 19:02:52 +0200

    feature: responsive product list

--- src/features/products/components/table/products-table.tsx
+++ src/features/products/components/table/products-table.tsx
@@ -3,6 +3,8 @@ import { Table } from "@/components/ui/table";
 import ProductsTableHeader from "./products-table-header";
 import ProductsTableBody from "./products-table-body";
 import ProductsTableFooter from "./products-table-footer";
+import ProductsCards from "./products-cards";
+import ProductsPagination from "./products-pagination";
 import { Card } from "@/components/ui/card";
 import { parseAsInteger, useQueryState } from "nuqs";
 import { getProducts } from "../../api";
@@ -19,17 +21,29 @@ function ProductsTable() {
   } = getProducts(products, { page, pageSize: 5 });
 
   return (
-    <Card className="overflow-hidden py-0">
-      <Table>
-        <ProductsTableHeader />
-        <ProductsTableBody products={data} />
-        <ProductsTableFooter
+    <>
+      <Card className="hidden overflow-hidden py-0 md:flex">
+        <Table>
+          <ProductsTableHeader />
+          <ProductsTableBody products={data} />
+          <ProductsTableFooter
+            page={currentPage}
+            pageSize={pageSize}
+            total={total}
+          />
+        </Table>
+      </Card>
+
+      <div className="flex flex-col gap-6 md:hidden">
+        <ProductsCards products={data} />
+        <ProductsPagination
           page={currentPage}
           pageSize={pageSize}
           total={total}
+          className="flex-col gap-3.5"
         />
-      </Table>
-    </Card>
+      </div>
+    </>
   );
 }
 
